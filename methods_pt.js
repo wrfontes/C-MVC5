a0c22957713f5f566b0c8288fb1519ee2571a497
wrfontes <wrfontes@gmail.com>
2017-06-05 23:45:57 -0300

    Update methods_pt.js

--- methods_pt.js
+++ methods_pt.js
@@ -1,3 +1,7 @@
+/*WFR
+ * Localized default methods for the jQuery validation plugin.
+ * Locale: PT_BR
+ */
 jQuery.extend(jQuery.validator.methods, {
     date: function (value, element) {
         return this.optional(element) || /^\d\d?\/\d\d?\/\d\d\d?\d?$/.test(value);
